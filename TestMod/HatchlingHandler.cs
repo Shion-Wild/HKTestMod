68e078740529994d1255df112bcf00f857a07f8e
Dillon <dillonwiltshire@gmail.com>
2025-08-06 23:59:07 -0400

    Hatchling manipulation and spawning

---
+++ TestMod/HatchlingHandler.cs
@@ -0,0 +1,82 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using GlobalEnums;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Satchel;
+using UnityEngine.PlayerLoop;
+using HutongGames.PlayMaker.Actions;
+
+namespace TestMod
+{
+    public class HatchlingHandler
+    {
+        private TestModMod TestModRef;
+
+        private int hatchlingCount = 0;
+
+        private GameObject hatchlingRef = new GameObject();        
+        private PlayMakerFSM hatchlingFSM = new PlayMakerFSM();
+        private KnightHatchling hatchlingComp = new KnightHatchling();
+
+        private Vector3 spawnPosition;
+
+        public HatchlingHandler(TestModMod TestModRef)
+        {
+            this.TestModRef = TestModRef;
+        }
+
+        public void Setup(GameObject charmEffects)
+        {
+            //Get a reference to the hatchling spawn FSM
+            hatchlingFSM = charmEffects.LocateMyFSM("Hatchling Spawn");
+            hatchlingComp = hatchlingFSM.GetComponent<KnightHatchling>();
+            
+            //Get a reference to the hatchling game object and add it to the preload dictionary
+            hatchlingRef = hatchlingFSM.GetFirstActionOfType<SpawnObjectFromGlobalPool>("Hatch").gameObject.Value;
+            TestModRef.preloads.Add("Hatchling", hatchlingRef);
+
+            //This modifies the existing charm's max hatchling count to 10
+            hatchlingFSM.InsertAction("Equipped", new SetIntValue { intVariable = hatchlingFSM.Fsm.GetFsmInt("Hatchling Max"), intValue = 10, everyFrame = false }, 0);
+        }
+
+        public void TrySpawnHatchling()
+        {
+            if (hatchlingRef != null)
+            {
+                if (hatchlingCount < 8)
+                {
+                    //Get knight position to spawn the hatchlings
+                    //spawnPosition = HeroController.instance.transform.position;
+                    //TestModRef.Log(HeroController.instance.transform.position + " - " + spawnPosition);
+
+                    TestModRef.Log("Trying to Spawn Hatchling");
+
+                    //Set spawn state to check for hatching conditions
+                    hatchlingFSM.SetState("Can Hatch?");
+
+                    /* Force spawn individual hatchlings
+                    GameObject hatchlingObj = GameObject.Instantiate(TestModRef.preloads["Hatchling"]);
+                    
+                    //Directly modify hatchling states and bools by getting a reference to its unique state machine component
+                    var hatchComponent = hatchlingObj.GetComponent<KnightHatchling>();
+                    hatchComponent.FsmQuickSpawn();
+                    hatchlingObj.transform.position = spawnPosition;
+                    hatchlingObj.SetActive(true);
+                    */
+
+                    //Increment Hatchling count
+                    //hatchlingCount += 1;
+                }
+                else
+                {
+                    TestModRef.Log("Too many hatchlings");
+                }
+            }
+        }
+    }
+}
--- TestMod/TestMod.cs
+++ TestMod/TestMod.cs
@@ -1,11 +1,31 @@
 using Modding;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Reflection;
+//using GlobalEnums;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
+//using Satchel;
+//using UnityEngine.PlayerLoop;
+//using HutongGames.PlayMaker.Actions;
+//using HKMirror;
+//using HKMirror.Hooks.ILHooks;
+using MonoMod.Cil;
 
 namespace TestMod
 {
-    public class TestModMod : Mod
+    public class TestModMod : Mod, ITogglableMod, IGlobalSettings<GlobalSettings>
     {
         private static TestModMod? _instance;
+        public GameObject crawlerPrefab;
+        public Dictionary<string, GameObject> preloads = new Dictionary<string, GameObject>();
+        public HatchlingHandler hatchlingHandler;
+
+        public GlobalSettings Settings { get; private set; } = new GlobalSettings();
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
 
         internal static TestModMod Instance
         {
@@ -19,20 +39,105 @@ namespace TestMod
             }
         }
 
-        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
-
         public TestModMod() : base("TestMod")
         {
             _instance = this;
+            hatchlingHandler = new HatchlingHandler(_instance);
         }
 
-        public override void Initialize()
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             Log("Initializing");
 
-            // put additional initialization logic here
+            //Don't set preload objects if preloads already exist
+            if (preloadedObjects != null)
+            {
+                // Preload entities into a public dictionary for spawning purposes
+                var crawlerPrefab = preloadedObjects["Crossroads_01"]["_Enemies/Crawler 1"];
+                preloads.Add("Crawler", crawlerPrefab);
+            }
+
+            RegisterCallbacks();
 
             Log("Initialized");
         }
+
+
+        public void RegisterCallbacks()
+        {
+            ModHooks.NewGameHook += OnNewGame;
+            On.NailSlash.StartSlash += StartSlash;
+            ModHooks.HeroUpdateHook += HeroUpdate;
+            ModHooks.AfterSavegameLoadHook += AfterSavegameLoad;
+            IL.HeroController.TakeDamage += GrubsongChanges;
+        }
+
+        public override List<(string, string)> GetPreloadNames()
+        {
+            return new List<(string, string)>
+            {
+                ("Crossroads_01", "_Enemies/Crawler 1")
+            };
+        }
+
+        private static void OnNewGame()
+        {
+            //PlayerData.instance.maxHealthBase = PlayerData.instance.maxHealth = PlayerData.instance.health = 8;
+            //PlayerData.instance.charmSlots += 2;
+        }
+
+        public void Unload()
+        {
+            ModHooks.NewGameHook -= OnNewGame;
+            On.NailSlash.StartSlash -= StartSlash;
+            ModHooks.HeroUpdateHook -= HeroUpdate;
+            ModHooks.AfterSavegameLoadHook -= AfterSavegameLoad;
+        }
+
+        public void AfterSavegameLoad(SaveGameData data)
+        {
+            GameManager.instance.StartCoroutine(HeroFinder());
+        }
+
+        private IEnumerator HeroFinder()
+        {
+            //Wait until hero is loaded
+            yield return new WaitWhile(() => HeroController.instance == null);
+
+            //Get a reference to the charm effects object
+            var charmEffects = GameObject.Find("Charm Effects");
+
+            //Set up Hatchling Handler
+            hatchlingHandler.Setup(charmEffects);
+        }
+
+        public void GrubsongChanges(ILContext il)
+        {
+            Log("Took damage");
+        }
+
+        public void HeroUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Log("Space was pressed");
+                if (PlayerData.instance.GetBool("equippedCharm_22"))
+                {
+                    Log("Charm is equipped");
+                    hatchlingHandler.TrySpawnHatchling();
+                }               
+            }
+        }
+
+        public void StartSlash(On.NailSlash.orig_StartSlash orig, NailSlash self)
+        {
+            orig(self);                       
+        }
+        
+
+        public void OnLoadGlobal(GlobalSettings s) => Settings = s;
+
+        public GlobalSettings OnSaveGlobal() => Settings;
+        
     }
 }
